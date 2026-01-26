34450fe7e859dd85ec1ecbf924fd3b0f89c0b97b
nazir1362002 <nazirkhan1362002@gmail.com>
2026-01-26 22:09:37 +0600

    Updated

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
-router.get('/',(req,res) => {
-    res.send('Task routes are Working!!')
+
+router.get('/test', auth , (req,res) => {
+    res.json({
+        message : "Task are working!!",
+        user : req.user
+    });
 });
 
 module.exports = router;
