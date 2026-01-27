8a16b774adcce9205d2c240b90f301a758f2d703
nazir1362002 <nazirkhan1362002@gmail.com>
2026-01-27 22:22:19 +0600

    Add task model schema

---
+++ models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const taskSchema = new mongoose.Schema({
+    taskName : {type:String,require:true},
+    description : {type:String,require:true},
+    completed: {type: Boolean, default:false},
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        require:true,
+        ref:'User'
+    }
+},
+{
+    timestamps:true
+});
+
+
+
+const Task = mongoose.model('Task',taskSchema);
+module.exports = Task;
